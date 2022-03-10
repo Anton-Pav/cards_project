8a574b2602f097e6e407b9fc0fa4a8c65a069415
Anton <ant.pavlov93@yandex.ru>
2022-03-10 12:33:45 +0300

    init commit

---
+++ src/main/ui/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            {/*hr, pr*/}
+            <>
+                {/*<Main/>*/}
+            </>
+        </div>
+    );
+}
+
+export default App;
